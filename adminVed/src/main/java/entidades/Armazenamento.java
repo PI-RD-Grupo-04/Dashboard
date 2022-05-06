0a520af20fb5aff9538e0d08a157782c68ca5499
Will <wb.premiertech@gmail.com>
2022-05-06 17:14:08 -0300

    correção geral

---
+++ adminVed/src/main/java/entidades/Armazenamento.java
@@ -0,0 +1,32 @@
+package entidades;
+
+public class Armazenamento {
+
+	private Integer id;
+	private String descricao;
+
+	public Armazenamento(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return descricao;
+	}
+}
